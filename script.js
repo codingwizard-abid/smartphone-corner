8bba87b9022a21c955804ccd1e28ae7da9e5bf56
Mohammad Abid <68217349+abidzmohammad@users.noreply.github.com>
2022-03-01 08:57:59 +0600

    final

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const displayItem = ()=> {
    const url = `https://openapi.programming-hero.com/api/phones?search=${inputFieldValue}`
    fetch(url)
    .then(res => res.json())
-   .then(data => showResultsOnUi(data.data));
+   .then(data => showResultsOnUi(data));
    inputField.value = '';
 }
 
@@ -16,26 +16,49 @@ const singlePhoneDetails = singlePhone =>{
    let url = `https://openapi.programming-hero.com/api/phone/${singlePhone}`;
    fetch(url)
    .then(response => response.json())
-   .then(data => console.log(data))
+   .then(data => console.log(data.data))
 }
 
 // show results on ui 
 const showResultsOnUi = (phones) => {
    const resultsWrapper = document.getElementById('results-wrapper');
-   console.log(phones);
-   phones.forEach(item => {
-      const div = document.createElement('div');
-      div.classList.add('col');
-      div.innerHTML = `
-      <div class="card h-100 p-3 rounded-3 shadow-sm">
-         <img src="${item.image}" class="card-img-top w-50 mx-auto" alt="${item.phone_name}">
-         <div class="card-body  text-center">
-            <h5 class="card-title">Model: ${item.phone_name}</h5>
-            <h6 class="card-title">Brand: ${item.brand}</h6>
+   resultsWrapper.textContent = '';
+   const phoneList = phones.data;
+   console.log(phoneList);
+   // if the result more that 20 
+   if(phoneList.length > 20){
+      const sliced = phoneList.slice(0,20);
+      sliced.forEach(item => {
+         const div = document.createElement('div');
+         div.classList.add('col');
+         div.innerHTML = `
+         <div class="card h-100 p-3 rounded-3 shadow-sm">
+            <img src="${item.image}" class="card-img-top w-50 mx-auto" alt="${item.phone_name}">
+            <div class="card-body  text-center">
+               <h5 class="card-title">Model: ${item.phone_name}</h5>
+               <h6 class="card-title">Brand: ${item.brand}</h6>
+            </div>
+            <button class="btn btn-dark" onclick="singlePhoneDetails('${item.slug}')">Explore more</button>
          </div>
-         <button class="btn btn-dark" onclick="singlePhoneDetails('${item.slug}')">Explore more</button>
-      </div>
-      `
-      resultsWrapper.appendChild(div)
-   })
+         `
+         resultsWrapper.appendChild(div);
+      })
+   }else{
+      phoneList.forEach(item => {
+         const div = document.createElement('div');
+         div.classList.add('col');
+         div.innerHTML = `
+         <div class="card h-100 p-3 rounded-3 shadow-sm">
+            <img src="${item.image}" class="card-img-top w-50 mx-auto" alt="${item.phone_name}">
+            <div class="card-body  text-center">
+               <h5 class="card-title">Model: ${item.phone_name}</h5>
+               <h6 class="card-title">Brand: ${item.brand}</h6>
+            </div>
+            <button class="btn btn-dark" onclick="singlePhoneDetails('${item.slug}')">Explore more</button>
+         </div>
+         `
+         resultsWrapper.appendChild(div);
+      })
+   }
+
 }
