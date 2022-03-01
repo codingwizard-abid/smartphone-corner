db383a8a53127b1e32c7725b4ae9e3ed610e78a9
Mohammad Abid <68217349+abidzmohammad@users.noreply.github.com>
2022-03-01 09:16:15 +0600

    phone listing

--- script.js
+++ script.js
@@ -25,40 +25,46 @@ const showResultsOnUi = (phones) => {
    resultsWrapper.textContent = '';
    const phoneList = phones.data;
    console.log(phoneList);
-   // if the result more that 20 
-   if(phoneList.length > 20){
-      const sliced = phoneList.slice(0,20);
-      sliced.forEach(item => {
-         const div = document.createElement('div');
-         div.classList.add('col');
-         div.innerHTML = `
-         <div class="card h-100 p-3 rounded-3 shadow-sm">
-            <img src="${item.image}" class="card-img-top w-50 mx-auto" alt="${item.phone_name}">
-            <div class="card-body  text-center">
-               <h5 class="card-title">Model: ${item.phone_name}</h5>
-               <h6 class="card-title">Brand: ${item.brand}</h6>
-            </div>
-            <button class="btn btn-dark" onclick="singlePhoneDetails('${item.slug}')">Explore more</button>
-         </div>
-         `
-         resultsWrapper.appendChild(div);
-      })
-   }else{
-      phoneList.forEach(item => {
-         const div = document.createElement('div');
-         div.classList.add('col');
-         div.innerHTML = `
-         <div class="card h-100 p-3 rounded-3 shadow-sm">
-            <img src="${item.image}" class="card-img-top w-50 mx-auto" alt="${item.phone_name}">
-            <div class="card-body  text-center">
-               <h5 class="card-title">Model: ${item.phone_name}</h5>
-               <h6 class="card-title">Brand: ${item.brand}</h6>
-            </div>
-            <button class="btn btn-dark" onclick="singlePhoneDetails('${item.slug}')">Explore more</button>
-         </div>
-         `
-         resultsWrapper.appendChild(div);
-      })
-   }
+   if(phones.status){
+      // result found remove error
+      document.getElementById('error').style.display = 'none';
 
+      // if the result more that 20 
+      if(phoneList.length > 20){
+         const sliced = phoneList.slice(0,20);
+         sliced.forEach(item => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100 p-3 rounded-3 shadow-sm">
+               <img src="${item.image}" class="card-img-top w-50 mx-auto" alt="${item.phone_name}">
+               <div class="card-body  text-center">
+                  <h5 class="card-title">Model: ${item.phone_name}</h5>
+                  <h6 class="card-title">Brand: ${item.brand}</h6>
+               </div>
+               <button class="btn btn-dark" onclick="singlePhoneDetails('${item.slug}')">Explore more</button>
+            </div>
+            `
+            resultsWrapper.appendChild(div);
+         })
+      }else{
+         phoneList.forEach(item => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100 p-3 rounded-3 shadow-sm">
+               <img src="${item.image}" class="card-img-top w-50 mx-auto" alt="${item.phone_name}">
+               <div class="card-body  text-center">
+                  <h5 class="card-title">Model: ${item.phone_name}</h5>
+                  <h6 class="card-title">Brand: ${item.brand}</h6>
+               </div>
+               <button class="btn btn-dark" onclick="singlePhoneDetails('${item.slug}')">Explore more</button>
+            </div>
+            `
+            resultsWrapper.appendChild(div);
+         })
+      }
+   }else{
+      document.getElementById('error').style.display = 'block';
+   }
 }
